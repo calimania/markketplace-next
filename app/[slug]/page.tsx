8104aa0ab1e5fc2c43d34b690f93b094d630e6ed
Daveed <daveed@santocabron.com>
2025-02-07 17:28:08 -0500

    Improved homepage links to be dynamic

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -16,7 +16,7 @@ const getCollection = async (key: string) => {
   }
 
   if (key === 'docs') {
-    const response = await strapiClient.getPosts({ page: 1, pageSize: 30 }, { filter: '', sort: 'updatedAt:desc' });
+    const response = await strapiClient.getPosts({ page: 1, pageSize: 30 }, { filter: '', sort: 'createdAt:desc' });
     collection = response?.data as Store[] || [];
   }
 
