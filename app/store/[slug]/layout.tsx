6c1f6e7fea1ce33aa0e46b64919dc6fbfe49e5e1
Daveed <daveed@santocabron.com>
2025-02-22 01:01:49 -0500

    using layuouts to display a common page for the /store pages

---
+++ app/store/[slug]/layout.tsx
@@ -0,0 +1,23 @@
+import {strapiClient} from "@/markket/api";
+import { ClientLayout } from "@/app/components/layout/store.layout";
+
+interface LayoutProps {
+  children: React.ReactNode;
+  params: { slug: string }; // Next.js automatically passes params
+}
+
+async function getStore(slug: string) {
+  'use server';
+  const response = await strapiClient.getStore(slug);
+  return response?.data?.[0];
+}
+
+export default async function StoreLayout({ children, params }: LayoutProps) {
+  const store = await getStore(params.slug);
+
+  return (
+    <ClientLayout store={store}>
+      {children}
+    </ClientLayout>
+  );
+};
