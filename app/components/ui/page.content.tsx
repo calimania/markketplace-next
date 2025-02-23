6ee2c4a3e8c8e91b7ce8f6fff2279d8285109876
Daveed <daveed@santocabron.com>
2025-02-22 19:51:34 -0500

    cleaned blog/id page

--- app/components/ui/page.content.tsx
+++ app/components/ui/page.content.tsx
@@ -4,16 +4,18 @@ import {
   type BlocksContent,
 } from "@strapi/blocks-react-renderer";
 import  { Page } from "@/markket/page.d";
+import { Article } from '@/markket/article';
 
 interface PageContentProps {
   params: {
     page?: Page;
+    post?: Article;
   };
 }
 
 export default function PageContent({ params }: PageContentProps) {
 
-  const content = params?.page?.Content;
+  const content = params?.page?.Content || params?.post?.Content;
 
   if (!content?.length) {
     return null;
