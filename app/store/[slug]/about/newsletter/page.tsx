bd92684c2fe2ceda1b654a8e99e4ea2325131cdb
Daveed <daveed@santocabron.com>
2025-02-22 19:33:13 -0500

    Using the metadata utils in newsletter

--- app/store/[slug]/about/newsletter/page.tsx
+++ app/store/[slug]/about/newsletter/page.tsx
@@ -1,31 +1,44 @@
 import { Stack, Title, LoadingOverlay } from '@mantine/core';
 import { strapiClient } from '@/markket/api';
-import { BlocksRenderer, type BlocksContent } from '@strapi/blocks-react-renderer';
 import { SubscribeForm } from '@/app/components/ui/subscribe.form';
 import { Store } from '@/markket/store';
 import { Suspense } from 'react';
-
+import PageContent from '@/app/components/ui/page.content';
+import { generateSEOMetadata } from '@/markket/metadata';
+import { Page } from "@/markket/page";
+import { Metadata } from "next";
 
 interface NewsletterPageProps {
   params: Promise<{  slug: string }>;
 }
 
-export default async function Page({ params }: NewsletterPageProps) {
+export async function generateMetadata({ params }: NewsletterPageProps): Promise<Metadata> {
+  const { slug, } = await params;
+
+  const response = await strapiClient.getPage('newsletter', slug);
+  const page = response?.data?.[0] as Page;
+
+  return generateSEOMetadata({
+    slug,
+    entity: {
+      url: `/${slug}`,
+      SEO: page?.SEO,
+    },
+    type: 'article',
+  });
+};
+
+export default async function NewsletterPage({ params }: NewsletterPageProps) {
   const { slug } = await params;
-
-  // @TODO: page data not returning when using this method?
-  const { data: [page]} = await strapiClient.getPage('newsletter', slug);
-  // @TODO: this page is calling this endpoint twice, in a higher up component already
+  const { data: [page] } = await strapiClient.getPage('newsletter', slug);
   const { data: [store] } = await strapiClient.getStore(slug);
-
   const title = page?.Title || `Newsletter for ${store?.SEO?.metaTitle}`;
-
   const image = page?.SEO?.socialImage || store?.SEO?.socialImage;
 
   return (
     <Stack gap="xl">
       <Title>{title}</Title>
-      <BlocksRenderer content={page?.Content as BlocksContent || []} />
+      <PageContent params={{ page }} />
       <Suspense fallback={<LoadingOverlay visible />}>
         <SubscribeForm
           store={store as Store}
