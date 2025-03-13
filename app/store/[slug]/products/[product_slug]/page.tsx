23cf3944da84121261c33d45fc95876582655291
Daveed <daveed@santocabron.com>
2025-03-12 22:59:33 -0400

    metadata in individaul product page

--- app/store/[slug]/products/[product_slug]/page.tsx
+++ app/store/[slug]/products/[product_slug]/page.tsx
@@ -2,11 +2,28 @@ import { strapiClient } from "@/markket/api.strapi";
 import { Product } from "@/markket/product";
 import ProductDisplay from '@/app/components/ui/product.display';
 import { Page } from "@/markket/page";
-
+import { Metadata } from "next";
+import { generateSEOMetadata } from "@/markket/metadata";
 
 interface ProductSlugPageProps {
   params: Promise<{ slug: string; product_slug: string }>;
 }
+
+export async function generateMetadata({ params }: ProductSlugPageProps): Promise<Metadata> {
+  const { slug, product_slug } = await params;
+  const { data } = await strapiClient.getProduct(product_slug, slug);
+  const page = data?.[0] as Page;
+
+  return generateSEOMetadata({
+    slug: `store/${slug}/products/${product_slug}`,
+    entity: {
+      url: `store/${slug}/products/${product_slug}`,
+      SEO: page?.SEO,
+      id: page?.id?.toString(),
+    },
+    defaultTitle: `Products`,
+  });
+};
 
 export default async function ProductSlugPage({ params }: ProductSlugPageProps) {
   const { slug, product_slug } = await params;
