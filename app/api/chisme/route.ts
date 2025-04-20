cbedfbe934aaf8045287f416412cc05df5671945
Daveed <daveed@santocabron.com>
2025-04-20 16:48:06 -0400

    fixed release_id error production chisme

--- app/api/chisme/route.ts
+++ app/api/chisme/route.ts
@@ -1,11 +1,21 @@
-import News from '@/app/utils/cision';
-import { NextResponse } from 'next/server';
+import News, { Release } from '@/app/utils/cision';
+import { NextResponse, NextRequest } from 'next/server';
 
-export async function GET() {
-  const news = await News.get();
+export async function GET(request: NextRequest) {
 
-  return NextResponse.json(
-    news,
-    { status: 200 }
-  )
+  const release_id = request?.nextUrl?.searchParams.get('release_id');
+
+  if (!release_id) {
+    const news = await News.get();
+    return NextResponse.json(
+      news,
+      { status: 200 }
+    )
+  }
+  const _release = await News.get_by_id(release_id);
+  const release = _release?.data as Release;
+
+  return NextResponse.json({
+    release
+  })
 };
--- app/components/chisme/release.page.tsx
+++ app/components/chisme/release.page.tsx
@@ -1,19 +1,48 @@
+'use client';
+
 import { formatReleaseDate, Release } from '@/app/utils/cision';
-import { Title, Container, Paper, Text, Badge, Group, Button, Stack, Box } from "@mantine/core";
+import { Title, Container, Paper, Text, Badge, Group, Button, Stack, Box, Skeleton } from "@mantine/core";
 import Link from 'next/link';
 import { IconArrowLeft, IconBrandGoogle } from '@tabler/icons-react';
+import { useState, useEffect } from 'react';
 
 type Props =  {
-  release: Release,
+  // release: Release,
+  release_id: string,
 }
 
-const ReleasePage = ({release}: Props) => {
+const ReleasePage = ({ release_id }: Props) => {
+  const [loading, setLoading] = useState(true);
+  const [release, setRelease] = useState({} as Release);
+
+  const getData = async () => {
+    const response = await fetch(`/api/chisme?release_id=${release_id}`);
+    const json = await response.json();
+    setRelease(json?.release)
+    setLoading(false);
+  }
+
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  if (loading) {
+    return (
+      <Container>
+        <Skeleton height={50} circle mb="xl" />
+        <Skeleton height={32} radius="xs" />
+        <Skeleton height={32} mt={6} radius="xs" />
+        <Skeleton height={32} mt={6} width="70%" radius="xs" />
+      </Container>
+    )
+  }
 
   return (
     <Container my="lg">
       <Stack gap="lg">
         <Group>
-          <Link href={`/chisme#${release.release_id}`} passHref>
+          <Link href={`/chisme#${release_id}`} passHref>
             <Button leftSection={<IconArrowLeft size={14} />} variant="outline">
               Back to Chisme
             </Button>
@@ -31,21 +60,17 @@ const ReleasePage = ({release}: Props) => {
             Read more
           </Button>
         </Group>
-
         <Paper shadow="xs" p="md" withBorder>
           <Stack gap="md">
             <Title order={2}>{release?.title}</Title>
-
             <Group>
               <Text size="sm" c="dimmed">Published:</Text>
               <Text size="sm">{formatReleaseDate(release.date)}</Text>
             </Group>
-
             <Group>
               <Text size="sm" c="dimmed">Source:</Text>
               <Text size="sm" fw={500}>{release?.source_company}</Text>
             </Group>
-
             {release?.geography?.length > 0 && (
               <Group gap="xs">
                 <Text size="sm" c="dimmed">Geography:</Text>
@@ -54,7 +79,6 @@ const ReleasePage = ({release}: Props) => {
                 ))}
               </Group>
             )}
-
             {release?.industry?.length > 0 && (
               <Group gap="xs">
                 <Text size="sm" c="dimmed">Industry:</Text>
@@ -63,7 +87,6 @@ const ReleasePage = ({release}: Props) => {
                 ))}
               </Group>
             )}
-
             {release?.subject?.length > 0 && (
               <Group gap="xs">
                 <Text size="sm" c="dimmed">Subject:</Text>
@@ -72,13 +95,32 @@ const ReleasePage = ({release}: Props) => {
                 ))}
               </Group>
             )}
-
             <Box style={{ fontSize: 'var(--mantine-font-size-md)', lineHeight: 'var(--mantine-line-height-md)' }}>
               <style dangerouslySetInnerHTML={{ __html: release?.style || '' }} />
               <div dangerouslySetInnerHTML={{ __html: release?.body || '' }}></div>
             </Box>
-
           </Stack>
+
+          <Group>
+            {!!release.multimedia && (
+              <div style={{ overflow: 'hidden', whiteSpace: 'nowrap', width: '100%' }}>
+                {release.multimedia.map((m, i) => (
+                  <img
+                    src={m.thumbnailurl}
+                    alt={m.caption}
+                    key={i}
+                    style={{
+                      display: 'inline-block',
+                      width: '360px',
+                      objectFit: 'cover',
+                      marginRight: '8px',
+                      borderRadius: '8px',
+                    }}
+                  />
+                ))}
+              </div>
+            )}
+          </Group>
         </Paper>
       </Stack>
     </Container>
